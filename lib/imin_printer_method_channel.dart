f9192166b956a7c1492d63d6909207416a11719a
SyahmiSAM <syahmisenpai97@gmail.com>
2025-02-09 21:45:05 +0800

    remove logger

--- lib/imin_printer_method_channel.dart
+++ lib/imin_printer_method_channel.dart
@@ -6,9 +6,9 @@ import 'enums.dart';
 import 'column_maker.dart';
 import 'imin_style.dart';
 import 'imin_printer_platform_interface.dart';
-import 'package:logger/logger.dart';
+// import 'package:logger/logger.dart';
 
-var logger = Logger();
+// var logger = Logger();
 
 /// An implementation of [IminPrinterPlatform] that uses method channels.
 class MethodChannelIminPrinter extends IminPrinterPlatform {
@@ -37,7 +37,7 @@ class MethodChannelIminPrinter extends IminPrinterPlatform {
   @override
   Future<Map<String, dynamic>> getPrinterStatus() async {
     final code = await methodChannel.invokeMethod<String>('getPrinterStatus');
-    logger.d('code $code');
+    // logger.d('code $code');
     Map<String, dynamic> printerStatus = <String, dynamic>{
       "code": code,
       "msg": PrinterStatus.getValue(code ?? '-1')
@@ -97,7 +97,7 @@ class MethodChannelIminPrinter extends IminPrinterPlatform {
 
   @override
   Future<void> setTextStyle(IminFontStyle style) async {
-    logger.d('setTextStyle');
+    // logger.d('setTextStyle');
     Map<String, dynamic> arguments = <String, dynamic>{
       "style": style.index,
     };
@@ -270,7 +270,7 @@ class MethodChannelIminPrinter extends IminPrinterPlatform {
         }
       }
     }
-    logger.d('printBarCode: $arguments');
+    // logger.d('printBarCode: $arguments');
     await methodChannel.invokeMethod<void>('printBarCode', arguments);
   }
 
@@ -705,38 +705,38 @@ class MethodChannelIminPrinter extends IminPrinterPlatform {
         arguments.putIfAbsent('text', () => '$text\n');
       }
       if (style.fontSize != null) {
-        logger.d('fontSize');
+        // logger.d('fontSize');
         await setTextBitmapSize(style.fontSize!);
       }
       if (style.typeface != null) {
-        logger.d('typeface');
+        // logger.d('typeface');
         await setTextBitmapTypeface(style.typeface!);
       }
       if (style.fontStyle != null) {
-        logger.d('fontStyle');
+        // logger.d('fontStyle');
         await setTextBitmapStyle(style.fontStyle!);
       }
       if (style.throughline != null) {
-        logger.d('throughline');
+        // logger.d('throughline');
         await setTextBitmapStrikeThru(style.throughline!);
       }
       if (style.underline != null) {
-        logger.d('underline');
+        // logger.d('underline');
         await setTextBitmapUnderline(style.underline!);
       }
 
       if (style.lineHeight != null) {
-        logger.d('lineHeight');
+        // logger.d('lineHeight');
         await setTextBitmapLineSpacing(style.lineHeight!);
       }
 
       if (style.letterSpacing != null) {
-        logger.d('letterSpacing');
+        // logger.d('letterSpacing');
         await setTextBitmapLetterSpacing(style.letterSpacing!);
       }
 
       if (style.reverseWhite != null) {
-        logger.d('reverseWhite');
+        // logger.d('reverseWhite');
         await setTextBitmapAntiWhite(style.reverseWhite!);
       }
     } else {
